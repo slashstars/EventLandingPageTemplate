238d553ab4600d6573cbf8498ebc1d90e43d12cd
Stoil Stoichev <stoilstoychev@gmail.com>
2016-02-11 14:58:18 +0100

    draggable: false

--- js/googleMapsInitializer.js
+++ js/googleMapsInitializer.js
@@ -24,7 +24,8 @@ function initialize(lat, lon) {
         center: new google.maps.LatLng(lat, lon),
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false
+        scrollwheel: false,
+		draggable: false
     }
     var map = new google.maps.Map(mapCanvas, mapOptions)
     var marker = new google.maps.Marker({
